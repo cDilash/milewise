22795af0fc98a75feeac42d64109f816deecaf38
Dilash Chimoriya <dc@Dilashs-MacBook-Pro.local>
2024-10-26 16:38:18 -0400

    Initial commit

--- js/config.js
+++ js/config.js
@@ -2,7 +2,12 @@ const config = {
     MAPBOX_TOKEN: 'pk.eyJ1IjoiZGlsYXNoMTAiLCJhIjoiY2x0MnBrNjNkMHMyZTJrcTNheGhpbDdycSJ9.pkMd94j1RxNhXOjhp9lD2A'
 };
 
-// For security in production
-if (window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1') {
-    config.MAPBOX_TOKEN = 'your_production_token'; // Your production token
+// For GitHub Pages
+if (window.location.hostname.includes('github.io')) {
+    config.MAPBOX_TOKEN = 'pk.eyJ1IjoiZGlsYXNoMTAiLCJhIjoiY2x0MnBrNjNkMHMyZTJrcTNheGhpbDdycSJ9.pkMd94j1RxNhXOjhp9lD2A';
+}
+
+// For local development
+if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+    config.MAPBOX_TOKEN = 'pk.eyJ1IjoiZGlsYXNoMTAiLCJhIjoiY2x0MnBrNjNkMHMyZTJrcTNheGhpbDdycSJ9.pkMd94j1RxNhXOjhp9lD2A';
 }
