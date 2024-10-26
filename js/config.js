960629f1dc2f9d86997e54db07a99308a7372356
Dilash Chimoriya <dc@Dilashs-MacBook-Pro.local>
2024-10-26 16:02:45 -0400

    Setup GitHub Pages

--- js/config.js
+++ js/config.js
@@ -1,1 +1,8 @@
- 
+const config = {
+    MAPBOX_TOKEN: 'pk.eyJ1IjoiZGlsYXNoMTAiLCJhIjoiY2x0MnBrNjNkMHMyZTJrcTNheGhpbDdycSJ9.pkMd94j1RxNhXOjhp9lD2A'
+};
+
+// For security in production
+if (window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1') {
+    config.MAPBOX_TOKEN = 'your_production_token'; // Your production token
+}
